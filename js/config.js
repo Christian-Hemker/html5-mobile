8ad19943fc905c9f9579598a9d9ded44209d94b2
Christian Hemker <Christian.Hemker@googlemail.com>
2012-01-11 14:10:30 +0100

    initial commit from https://code.google.com/p/bleedinghtml5/

---
+++ js/config.js
@@ -0,0 +1,24 @@
+window.slidesConfig = window.slidesConfig || {
+  // Slide settings
+  settings : {
+    useBuilds: true,
+    useGDDBranding: true,
+    includeWebRTC: false
+    //hashtag: '#html5'
+  },
+  info: {
+    // Personal info
+    name: '',
+    pic: '',
+    gplus: '',
+    twitter: '',
+    blog: '',
+
+    // GDD info
+    location: 'Berlin, Germany',
+    country: 'DE',
+    date: new Date('Nov 19 2011 15:00'),
+    //logo: 'images/chrome_logo.png'
+    logo: 'http://www.google.com/events/developerday/2011/static/img/event-berlin.png'
+  }
+};
